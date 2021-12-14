1a8905f36956548be761f04b52ec34e83082dc7b
kwant <user@macBookAir-kwant.local>
2021-12-14 08:56:02 +0200

    home-work-1_v1.0

--- src/home-work-1/components/Profile.jsx
+++ src/home-work-1/components/Profile.jsx
@@ -10,38 +10,38 @@ const setData = (data) => {
     .slice(0, -3);
 };
 
-// export default class Profile extends Component {
-//   render() {
-//     return (
-//       <div
-//         style={{
-//           border: '12px solid rgb(225,225,225)',
-//           borderRadius: 20,
-//           margin: 50,
-//           paddingLeft: 51,
-//         }}>
-//         <p style={{ fontSize: 50 }}>
-//           Привет, <b>{this.props.name}</b>!
-//         </p>
-//         <p style={{ fontSize: 35 }}>Дата регистрации: {setData(this.props.registredAt)}</p>
-//       </div>
-//     );
-//   }
-// }
-
-export default function Profile(props) {
-  return (
-    <div
-      style={{
-        border: '12px solid rgb(225,225,225)',
-        borderRadius: 20,
-        margin: 50,
-        paddingLeft: 51,
-      }}>
-      <p style={{ fontSize: 50 }}>
-        Привет, <b>{props.name}</b>!
-      </p>
-      <p style={{ fontSize: 35 }}>Дата регистрации: {setData(props.registredAt)}</p>
-    </div>
-  );
+export default class Profile extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          border: '12px solid rgb(225,225,225)',
+          borderRadius: 20,
+          margin: 50,
+          paddingLeft: 51,
+        }}>
+        <p style={{ fontSize: 50 }}>
+          Привет, <b>{this.props.name}</b>!
+        </p>
+        <p style={{ fontSize: 35 }}>Дата регистрации: {setData(this.props.registredAt)}</p>
+      </div>
+    );
+  }
 }
+
+// export default function Profile(props) {
+//   return (
+//     <div
+//       style={{
+//         border: '12px solid rgb(225,225,225)',
+//         borderRadius: 20,
+//         margin: 50,
+//         paddingLeft: 51,
+//       }}>
+//       <p style={{ fontSize: 50 }}>
+//         Привет, <b>{props.name}</b>!
+//       </p>
+//       <p style={{ fontSize: 35 }}>Дата регистрации: {setData(props.registredAt)}</p>
+//     </div>
+//   );
+// }
