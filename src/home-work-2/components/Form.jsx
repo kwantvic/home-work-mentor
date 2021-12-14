eaf478b516ed927ef5661be7bf2f4e8607d6c153
kwant <user@macBookAir-kwant.local>
2021-12-14 23:57:39 +0200

    v1.0

---
+++ src/home-work-2/components/Form.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const style = {
+  display: 'flex',
+  flexDirection: 'column',
+  width: 796,
+  margin: 'auto',
+  paddingTop: 50,
+};
+
+const style__input = {
+  width: 797,
+  height: 97,
+  marginBottom: 48,
+  fontSize: 62,
+};
+
+const style__button = {
+  width: 247,
+  height: 97,
+  fontSize: 62,
+};
+
+export default function Form() {
+  let email = '',
+    password = '';
+
+  function onChangeInput(e) {
+    e.target.name === 'email' ? (email = e.target.value) : (password = e.target.value);
+  }
+
+  function handleSubmit(e) {
+    !email || !password ? alert('❗️Заполните поля формы!') : console.log({ email, password });
+    e.preventDefault();
+    e.target[0].value = '';
+    e.target[1].value = '';
+    email = '';
+    password = '';
+  }
+
+  return (
+    <form onSubmit={handleSubmit} style={style}>
+      <input
+        onChange={onChangeInput}
+        style={style__input}
+        name="email"
+        type="text"
+        placeholder="E-Mail"
+      />
+      <input
+        onChange={onChangeInput}
+        style={style__input}
+        name="password"
+        type="password"
+        placeholder="Пароль"
+      />
+      <button style={style__button} type="submit">
+        Войти
+      </button>
+    </form>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './home-work-1/App';
+// import App from './home-work-1/App';
+import App from './home-work-2/App';
 
 ReactDOM.render(
   <React.StrictMode>
