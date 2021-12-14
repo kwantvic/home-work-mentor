42ae9e384a1e43113c5929426595acb5a586ba86
kwant <user@macBookAir-kwant.local>
2021-12-14 19:54:55 +0200

    v1.1 отделил имя

--- src/home-work-1/components/Profile.jsx
+++ src/home-work-1/components/Profile.jsx
@@ -1,47 +1,48 @@
 import React, { Component } from 'react';
 
-const setData = (data) => {
-  return data
+const setData = (data) =>
+  data
     .toLocaleString('ru-RU', {
       year: 'numeric',
       month: 'long',
       day: 'numeric',
     })
     .slice(0, -3);
-};
 
-export default class Profile extends Component {
-  render() {
-    return (
-      <div
-        style={{
-          border: '12px solid rgb(225,225,225)',
-          borderRadius: 20,
-          margin: 50,
-          paddingLeft: 51,
-        }}>
-        <p style={{ fontSize: 50 }}>
-          Привет, <b>{this.props.name}</b>!
-        </p>
-        <p style={{ fontSize: 35 }}>Дата регистрации: {setData(this.props.registredAt)}</p>
-      </div>
-    );
-  }
-}
+const firstName = (name) => name.split(' ')[0];
 
-// export default function Profile(props) {
-//   return (
-//     <div
-//       style={{
-//         border: '12px solid rgb(225,225,225)',
-//         borderRadius: 20,
-//         margin: 50,
-//         paddingLeft: 51,
-//       }}>
-//       <p style={{ fontSize: 50 }}>
-//         Привет, <b>{props.name}</b>!
-//       </p>
-//       <p style={{ fontSize: 35 }}>Дата регистрации: {setData(props.registredAt)}</p>
-//     </div>
-//   );
+// export default class Profile extends Component {
+//   render() {
+//     return (
+//       <div
+//         style={{
+//           border: '12px solid rgb(225,225,225)',
+//           borderRadius: 20,
+//           margin: 50,
+//           paddingLeft: 51,
+//         }}>
+//         <p style={{ fontSize: 50 }}>
+//           Привет, <b>{firstName(this.props.name)}</b>!
+//         </p>
+//         <p style={{ fontSize: 35 }}>Дата регистрации: {setData(this.props.registredAt)}</p>
+//       </div>
+//     );
+//   }
 // }
+
+export default function Profile(props) {
+  return (
+    <div
+      style={{
+        border: '12px solid rgb(225,225,225)',
+        borderRadius: 20,
+        margin: 50,
+        paddingLeft: 51,
+      }}>
+      <p style={{ fontSize: 50 }}>
+        Привет, <b>{firstName(props.name)}</b>!
+      </p>
+      <p style={{ fontSize: 35 }}>Дата регистрации: {setData(props.registredAt)}</p>
+    </div>
+  );
+}
